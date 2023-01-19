84691ade4231853f408197b8156c44564c0fd217
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2023-01-18 16:44:39 -0800

    added imports and started export funtion ContactSection

--- portfolio2/src/components/pages/contact.js
+++ portfolio2/src/components/pages/contact.js
@@ -1,2 +1,10 @@
 import React, {useState} from 'react';
 import { validateEmail, validateName, validateMessage} from '../../utils/helpers';
+import { send } from 'emailjs';
+
+export default function ContactSection(mrPresident) {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+}
