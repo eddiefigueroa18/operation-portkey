1d30f225519e5df5a061c5bac910eecd40d9a5d9
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2023-01-18 17:04:45 -0800

    added more syntax to contact.js. added handleInputChange. Next handleFormSubmit

--- portfolio2/src/components/pages/contact.js
+++ portfolio2/src/components/pages/contact.js
@@ -7,4 +7,43 @@ export default function ContactSection(mrPresident) {
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+
+
+
+    const [toSend, setToSend] = useState({
+        email: '',
+        name: '',
+        message: '',
+    });
+
+    //Sets the message requiring user input in each field
+    function requireField(e) {
+        if(!e.target.value) {
+            setErrorMessage(e.target.name + " This field is required");
+        }
+    }
+
+    //Set the name and the value for what exactly caused the change.
+    const handleInputChange = (e) => {
+        const { name, value, } = e.target;
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+
+        if (name === "name") {
+            setName(value);
+        } else if (name === "email") {
+            setEmail(value);
+        } else if (name === "message") {
+            setMessage(value);
+        }
+        return 
+    };
+
+
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
+
 }
