71405698c0d905a102b39fbb9c4907276b57296c
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2023-01-18 17:42:17 -0800

    added the input boxes for each contact section requirment. Need to style. Not complete

--- portfolio2/src/components/pages/contact.js
+++ portfolio2/src/components/pages/contact.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { validateEmail, validateName, validateMessage} from '../../utils/helpers';
-import { send } from 'emailjs';
+// import { send } from 'emailjs';
 
 export default function ContactSection(mrPresident) {
     const [email, setEmail] = useState('');
@@ -21,7 +21,7 @@ export default function ContactSection(mrPresident) {
         if(!e.target.value) {
             setErrorMessage(e.target.name + " This field is required");
         }
-    }
+    };
 
     //Set the name and the value for what exactly caused the change.
     const handleInputChange = (e) => {
@@ -39,11 +39,94 @@ export default function ContactSection(mrPresident) {
     };
 
 
-
+    // Validation for user input according to REGEX in helpers.js file
     const handleFormSubmit = (e) => {
         e.preventDefault();
-    }
+
+        if (!validateEmail(email)) {
+            setErrorMessage('Please enter a valid email!');
+            return;
+        }
+
+        if (!validateName(name)) {
+            setErrorMessage('Please enter a valid name!');
+            return;
+        }
+        if (!validateMessage(message)) {
+            setErrorMessage('Please enter a message 5 characters or more, Thank you!');
+            return;
+        }
+
+        //Clears all inputs
+        setErrorMessage('');
+        setEmail('');
+        setName('');
+        setMessage('');
+    };
+
+   //===================================================================================//
+
+   return (
+
+    <section className="contactMeContainer">
+        <h2 className='contactMeHeader'>Contact Me</h2>
+
+            <form className='form-group'>
+
+                {/* Name */}
+                <input 
+                className='nameInput'
+                onChange={handleInputChange}
+                onBlur={requireField}
+                value={name}
+                name="Name"
+                type="text"
+                placeholder="Your name"
+                />
+                <br />
+
+                {/* email */}
+                <input 
+                className='emailInput'
+                onChange={handleInputChange}
+                onBlur={requireField}
+                value={email}
+                name="email"
+                type="text"
+                placeholder="email"
+                />
+                <br />
+
+                {/* Message */}
+                <input 
+                className='messageInput'
+                onChange={handleInputChange}
+                onBlur={requireField}
+                value={message}
+                name="message"
+                type="text"
+                placeholder="Type your message here"
+                />
+                <br />
+
+                {/* submit button */}
+                <div>
+                    <button className="submit-button" type="button" onClick={handleFormSubmit}>Submit</button>
+                </div>
+
+                {/* error message text */}
+                {errorMessage && (
+                    <div>
+                        <p className='error-message-text'>{errorMessage}</p>
+                    </div>
+                )}
+
+
+            </form>
+
+    </section>
 
 
 
+   );
 }
