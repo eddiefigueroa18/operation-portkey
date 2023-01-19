85348d1bff0bd67ca05559bffe8ff70dd3ee2206
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2023-01-19 11:50:27 -0800

    adde more css. Looks better but still wonky. needs fixing

--- portfolio2/src/components/footer.js
+++ portfolio2/src/components/footer.js
@@ -12,22 +12,22 @@ function Footer() {
 
             {/* ========================= GMAIL ======================== */}
             <div className="contact-links">
-                <a href="https://github.com/eddiefigueroa18"  className="githubFooterLogo" target="blank">
-                    <img className="githubFooterLogo" src={githubLogo} alt="Github Logo"/>
+                <a href="https://github.com/eddiefigueroa18" className="githubFooterLogo" target="blank">
+                    <img className="githubFooterLogo" src={githubLogo} alt="Github Logo" />
                 </a>
             </div>
 
             {/* ========================= LINKED-IN ======================== */}
             <div className="contact-links">
                 <a href="https://www.linkedin.com/in/eduardo-figueroa-93b425235/" className="linkedFooterLogo" target="blank">
-                    <img className="linkedFooterLogo" src={linkedInLogo} alt="Linkedin Logo"/>
+                    <img className="linkedFooterLogo" src={linkedInLogo} alt="Linkedin Logo" />
                 </a>
             </div>
 
             {/* ========================= INSTAGRAM ======================== */}
             <div className="contact-links">
                 <a href="https://www.instagram.com/eddie_figueroa_/" className="instaFooterLogo" target="blank">
-                    <img className="instaFooterLogo"src={IgLogo} alt="Instagram Logo" />
+                    <img className="instaFooterLogo" src={IgLogo} alt="Instagram Logo" />
                 </a>
             </div>
 
@@ -37,38 +37,38 @@ function Footer() {
                     <img className="gmailFooterLogo" src={gmailLogo} alt="Gmail Logo" />
                 </a>
             </div>
-
-            <h3 className="footer-section">Eduardo Figueroa &copy;2023</h3>
+            {/* 
+            <h3 className="footer-section">Eduardo Figueroa &copy;2023</h3> */}
 
         </footer>
 
 
-//  <footer className="text-center text-white" style={{backgroundColor: '#252934'}}>
-//             <div className="container pt-4">
-//                 <section className="mb-4">
-//                     <a
-//                         className="btn text-white btn-floating m-1"
-//                         style={{backgroundColor: '1c9690'}}
-//                         href="mailto:eddiefigueroa18@gmail.com"
-//                         target='_blank'
-//                         role="button"
-//                         ><i class="bi bi-google"></i>
-//                     </a>
-//                     <a
-//                         className="btn text-white btn-floating m-1"
-//                         style={{backgroundColor: '187841'}}
-//                         href="https://www.linkedin.com/in/eduardo-figueroa-93b425235/"
-//                         target='_blank'
-//                         role="button"
-//                         ><i class="bi bi-linkedin"></i>
-//                     </a>
+        //  <footer className="text-center text-white" style={{backgroundColor: '#252934'}}>
+        //             <div className="container pt-4">
+        //                 <section className="mb-4">
+        //                     <a
+        //                         className="btn text-white btn-floating m-1"
+        //                         style={{backgroundColor: '1c9690'}}
+        //                         href="mailto:eddiefigueroa18@gmail.com"
+        //                         target='_blank'
+        //                         role="button"
+        //                         ><i class="bi bi-google"></i>
+        //                     </a>
+        //                     <a
+        //                         className="btn text-white btn-floating m-1"
+        //                         style={{backgroundColor: '187841'}}
+        //                         href="https://www.linkedin.com/in/eduardo-figueroa-93b425235/"
+        //                         target='_blank'
+        //                         role="button"
+        //                         ><i class="bi bi-linkedin"></i>
+        //                     </a>
 
 
-//                 </section>
-//             </div>
-//         </footer>  
+        //                 </section>
+        //             </div>
+        //         </footer>  
 
-             
+
     );
 }
 
--- portfolio2/src/components/pages/portfolio.js
+++ portfolio2/src/components/pages/portfolio.js
@@ -18,23 +18,24 @@ const myWork = () => {
             {/* ========================= ONE ======================== */}
             <div className="work1" id="workSectionImageLinks">
                 <h3 className="workTitle1" id="workTitles">Weather Dashboard</h3>
-                <a href="https://eddiefigueroa18.github.io/operation-nimbus/" target="blank" className="weatherIcon">
-                    <img src={weatherDashPhoto} alt="Image of weather forecast symbol (sun and clouds)" />
-                </a>
-                <a href="https://github.com/eddiefigueroa18/operation-nimbus" id="githubLogo" className="work1Github" target="blank">
-                    <img src={githubLogo} />
-                </a>
+                        <a href="https://eddiefigueroa18.github.io/operation-nimbus/" target="blank" >
+                            <img className="weatherIcon" src={weatherDashPhoto} alt="Image of weather forecast symbol (sun and clouds)" />
+                        </a>
+                        <a href="https://github.com/eddiefigueroa18/operation-nimbus" className="work1Github" id="githubLinks"  target="blank">Github Repository 
+                            {/* <img  src={githubLogo} alt="github logo"/> */}
+                        </a>
+                    
             </div>
 
 
             {/* ========================= TWO ======================== */}
             <div className="work2" id="workSectionImageLinks">
                 <h3 className="workTitle2" id="workTitles">Password Generator</h3>
-                <a href="https://eddiefigueroa18.github.io/operation-safeguard/" target="blank" className="passwordIcon">
-                    <img src={passwordGenPhoto} alt="Image of a padlock" />
+                <a href="https://eddiefigueroa18.github.io/operation-safeguard/" target="blank" >
+                    <img className="passwordIcon" src={passwordGenPhoto} alt="Image of a padlock" />
                 </a>
-                <a href="https://github.com/eddiefigueroa18/operation-safeguard" id="githubLogos" className="work2Github" target="blank">
-                    <img src={githubLogo} />
+                <a href="https://github.com/eddiefigueroa18/operation-safeguard" id="githubLinks"  target="blank">Github Repository
+                    {/* <img className="work2Github" src={githubLogo} /> */}
                 </a>
             </div>
 
@@ -42,11 +43,11 @@ const myWork = () => {
             {/* ========================= Three ======================== */}
             <div className="work3" id="workSectionImageLinks">
                 <h3 className="workTitle3" id="workTitles">Itinerary Maker</h3>
-                <a href="https://eddiefigueroa18.github.io/operation-itenerary/" target="blank" className="iteneraryIcon">
-                    <img src={iteneraryPhoto} alt="Image of a blank Checklist" />
+                <a href="https://eddiefigueroa18.github.io/operation-itenerary/" target="blank" >
+                    <img className="iteneraryIcon" src={iteneraryPhoto} alt="Image of a blank Checklist" />
                 </a>
-                <a href="https://github.com/eddiefigueroa18/operation-itenerary" id="githubLogos" className="work3Github" target="blank">
-                    <img src={githubLogo} />
+                <a href="https://github.com/eddiefigueroa18/operation-itenerary" id="githubLinks"  target="blank">Github Repository
+                    {/* <img className="work3Github" src={githubLogo} /> */}
                 </a>
             </div>
 
@@ -55,11 +56,11 @@ const myWork = () => {
             {/* THIS WEBSITE NEEDS TO BE FIXED ON HEROKU */}
             <div className="work4" id="workSectionImageLinks">
                 <h3 className="workTitle4" id="workTitles">Note Taker App</h3>
-                <a href="https://eddiefigueroa18.github.io/operation-itenerary/" target="blank" className="noteTakerIcon">
-                    <img src={noteTakerPhoto} alt="Image of sticky notes with pencil on top" />
+                <a href="https://eddiefigueroa18.github.io/operation-itenerary/" target="blank" >
+                    <img className="noteTakerIcon" src={noteTakerPhoto} alt="Image of sticky notes with pencil on top" />
                 </a>
-                <a href="https://github.com/eddiefigueroa18/operation-wordsmith" id="githubLogos" className="work4Github" target="blank">
-                    <img src={githubLogo} />
+                <a href="https://github.com/eddiefigueroa18/operation-wordsmith" id="githubLinks"  target="blank">Github Repository
+                    {/* <img className="work4Github" src={githubLogo} /> */}
                 </a>
             </div>
 
@@ -67,11 +68,11 @@ const myWork = () => {
             {/* ========================= FIVE ======================== */}
             <div className="work5" id="workSectionImageLinks">
                 <h3 className="workTitle5" id="workTitles">A Javasript Quiz App</h3>
-                <a href="https://eddiefigueroa18.github.io/operation-java-quiz/" target="blank" className="JavaIcon">
-                    <img src={javaQuizPhoto} alt="Image of mouse pointer selecting a quiz button" />
+                <a href="https://eddiefigueroa18.github.io/operation-java-quiz/" target="blank" >
+                    <img className="JavaIcon" src={javaQuizPhoto} alt="Image of mouse pointer selecting a quiz button" />
                 </a>
-                <a href="https://github.com/eddiefigueroa18/operation-java-quiz" id="githubLogos" className="work5Github" target="blank">
-                    <img src={githubLogo} />
+                <a href="https://github.com/eddiefigueroa18/operation-java-quiz" id="githubLinks"  target="blank">Github Repository
+                    {/* <img className="work5Github" src={githubLogo} /> */}
                 </a>
             </div>
 
@@ -79,11 +80,11 @@ const myWork = () => {
             {/* ========================= SIX ======================== */}
             <div className="work6" id="workSectionImageLinks">
                 <h3 className="workTitle6" id="workTitles">An event finder and hotel booker app</h3>
-                <a href="https://abrowncownow.github.io/event-booker/" target="blank" className="showHeroIcon">
-                    <img src={showHeroPhoto} alt="Image of standing crowd with raised hands" />
+                <a href="https://abrowncownow.github.io/event-booker/" target="blank" >
+                    <img className="showHeroIcon" src={showHeroPhoto} alt="Image of standing crowd with raised hands" />
                 </a>
-                <a href="https://github.com/abrowncownow/event-booker" id="githubLogos" className="work6Github" target="blank">
-                    <img src={githubLogo} />
+                <a href="https://github.com/abrowncownow/event-booker" id="githubLinks"  target="blank">Github Repository
+                    {/* <img className="work6Github" src={githubLogo} /> */}
                 </a>
             </div>
 
